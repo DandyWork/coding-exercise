0cc1e421cf6731bcb859ae25d8f2e2aa66a4d553
DandyWork <136493258+DandyWork@users.noreply.github.com>
2025-09-02 19:02:38 +0700

    Update palindrome.py
    adding explanation

--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,6 @@
+def is_palindrome(word):          # Define a function called is_palindrome that takes one parameter "word"
+    reversed_word = word[::-1]    # Reverse the string using slicing, word[start:stop:step], not mentioning start and stop will take a whole world , for step if negative means its reading right to left 
+    return word == reversed_word  # Compare original and reversed, return True if they are equal, otherwise False
 
+print(is_palindrome("madam"))   # True (because "madam" backwards is "madam")
+print(is_palindrome("hello"))   # False (because "hello" backwards is "olleh")
