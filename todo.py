4ce3d7a9e6be11a5f46d5c06ff8167f13d02d008
DandyWork <136493258+DandyWork@users.noreply.github.com>
2025-09-03 09:47:31 +0700

    Update todo.py

--- todo.py
+++ todo.py
@@ -1,21 +1,19 @@
-import requests  # Import the requests library to make HTTP requests
+import requests  # Import request library biar bisa pake http
 
-response = requests.get("https://jsonplaceholder.typicode.com/todos")
-# This sends a GET request to fetch all todo items.
+response = requests.get("https://jsonplaceholder.typicode.com/todos") # request GET dari http
 
-todos = response.json()
-# Parse the JSON response into a Python list of dictionaries.
+todos = response.json() # masukin JSON response dari request biar bisa dipake 
 
-total_todos = len(todos)
+total_todos = len(todos) #nyari totalnya
 print("Total number of todos:", total_todos)
 
-# Count completed todos in a simple way
+# Itung satu2 mana yang completed
 completed = 0  
 for todo in todos:  
     if todo["completed"] == True:  
         completed = completed + 1  
 
-not_completed = total_todos - completed 
+not_completed = total_todos - completed #sisa total sama completed jadi not completed
 
 print("Number of completed todos:", completed)
 print("Number of not completed todos:", not_completed)
